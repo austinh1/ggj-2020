a5d721105a7f7699c5c79f00b4ec722ed9330788
BrendanDickinson <brendangamer@gmail.com>
2020-02-01 10:41:54 -0500

    Camera now smoothly goes behind player no matter facing direction

---
+++ Assets/TransformFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFollow : MonoBehaviour
+{
+    public Transform transToFollow;
+
+    public bool followRotation;
+    public bool followPosition;
+    public bool smoothFollow;
+
+    public float rotSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followPosition)
+        {
+            transform.position = transToFollow.position;
+
+        }
+
+        if (followRotation)
+        {
+            if (!smoothFollow)
+            {
+                transform.rotation = transToFollow.rotation;
+            }
+            else if (smoothFollow)
+            {
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, transToFollow.localRotation, Time.deltaTime);
+            }
+        }
+        
+        
+    }
+}
