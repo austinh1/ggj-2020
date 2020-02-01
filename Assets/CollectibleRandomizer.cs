018d29a1e41173d1719c033d87616a8948c1b411
austinh1 <codexscript1@gmail.com>
2020-02-01 11:31:21 -0500

    Added lots of good stuff

---
+++ Assets/CollectibleRandomizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CollectibleRandomizer : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> collectiblePrefabs;
+    private void Start()
+    {
+        var prefab = collectiblePrefabs[0];
+        var collectible = Instantiate(prefab);
+
+        var sphereCollider = GetComponent<SphereCollider>();
+        var toSurface = sphereCollider.bounds.center;
+        toSurface.y += sphereCollider.radius;
+        // var rotated = Quaternion.Euler(0, 0, 0) * toSurface;
+
+        collectible.transform.position = toSurface;
+    }
+}
