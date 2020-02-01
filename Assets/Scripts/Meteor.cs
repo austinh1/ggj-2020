8913104d607dec4ff481c6adf13f922a10ce08cc
Andy <agergel@callibrity.com>
2020-02-01 15:56:52 -0800

    Create a randomized prefab. Spin those gastroids like a record baby, right round round round.

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
 
 public class Meteor : MonoBehaviour
 {
     private GameObject Planet { get; set; }
     
+    public float distToGround;
+
     [SerializeField]
     private float initialSpeed = 30;
+
+    private const String PLANET = "Planet";
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +27,20 @@ public class Meteor : MonoBehaviour
         tr.rotation = Quaternion.LookRotation(forward, -down);
         GetComponent<Rigidbody>().AddForce(tr.forward * initialSpeed, ForceMode.VelocityChange);
         Debug.DrawRay(transform.position, tr.forward * 50, Color.green);
+        var initialTorque = new Vector3(Random.Range(-1,1), Random.Range(-1,1), Random.Range(-1,1)) * 50;
+        GetComponent<Rigidbody>().AddTorque(initialTorque);
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //doesn't work, would like to fix if we have time for pollishing.
+        if(other.collider.tag.Equals(PLANET) && GetComponent<Rigidbody>().angularDrag != 0)
+        {
+            Debug.Log("HIT");
+            GetComponent<Rigidbody>().angularDrag = 1f;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/RandomMesh.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomMesh : MonoBehaviour
+{
+    [SerializeField]
+    private List<Model> listOfModels = new List<Model>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var modelIndex = Random.Range(0, listOfModels.Count);
+        var currentModel = listOfModels[modelIndex];
+        GetComponent<MeshFilter>().mesh = currentModel.mesh;
+        GetComponent<MeshRenderer>().material = currentModel.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Serializable]
+    class Model
+    {
+        public Mesh mesh;
+        public Material material;
+    }
+}
