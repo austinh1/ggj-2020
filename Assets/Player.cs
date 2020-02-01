6894d9de8d796b9c8dce60aa838e733079e7a113
austinh1 <codexscript1@gmail.com>
2020-01-31 22:52:47 -0500

    Rewired

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using Rewired;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rewired.Player Input { get; set; }
+    private CapsuleCollider CapsuleCollider { get; set; }
+    [SerializeField] private LayerMask WhatIsGround;
+    
+    private void Start()
+    {
+        Input = ReInput.players.GetPlayer(0);
+    }
+
+    private void Update()
+    {
+        RaycastHit[] raycastHits;
+        var size = Physics.RaycastNonAlloc(CapsuleCollider.bounds.center, -transform.up, out raycastHits, CapsuleCollider.height * 0.5f + .1f, WhatIsGround);
+    }
+}
