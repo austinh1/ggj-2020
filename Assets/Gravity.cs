04c6192cda64cd83e2b79ca85ef817d3affb2c23
BrendanDickinson <brendangamer@gmail.com>
2020-02-01 10:19:20 -0500

    Player direction facing working

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
- 
 // Anything with a rigidbody and inside the planets trigger collidor will be effected by gravity
- 
+
     float gravity = -9.81f;
-    void OnTriggerStay(Collider other) {
+
+    void OnTriggerStay(Collider other)
+    {
         // get the direction vector and normalize it (magnitude = 1)
         Vector3 direction = other.transform.position - transform.position;
         Vector3 force = direction.normalized * gravity;
- 
+
         // accelerate the object in that direction
-        other.GetComponent<Rigidbody>().AddForce(force, ForceMode.Acceleration);
+        Rigidbody rbody;
+        if ((rbody = other.GetComponent<Rigidbody>()) != null)
+            other.GetComponent<Rigidbody>().AddForce(force, ForceMode.Acceleration);
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,34 +15,6 @@ public class PlayerController : MonoBehaviour {
     public float rotationDamping;
     public Transform cameraTransform;
 
-//    void Start ()
-//    {
-//        rb = GetComponent<Rigidbody>();
-//        count = 0;
-//    }
-
-//    void FixedUpdate ()
-//    {
-//        float moveHorizontal = Input.GetAxis ("Horizontal");
-//        float moveVertical = Input.GetAxis ("Vertical");
-//
-//        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-//        //rb.velocity = movement * speed;
-//        
-//        RaycastHit hit = new RaycastHit();
-//        //Vector3 castPos = new Vector3(transform.position.x, transform.position.y - 0.25f, transform.position.z);
-//       
-//        if (Physics.Raycast(transform.position, -transform.up, out hit))
-//        {
-//            transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
-//            transform.position = hit.point + transform.up*GetComponent<CapsuleCollider>().height/2;
-//            Vector3 forceVector = Vector3.ProjectOnPlane(movement, hit.normal);
-//            rb.AddForce (forceVector * speed);
-//        }
-//        
-//
-//    }
-
     void Update () 
     {        
         Vector3 movementDirection = Vector3.zero;
