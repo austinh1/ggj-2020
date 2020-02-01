680a99628d976692cf78cc8523d1d0bd7cbff677
austinh1 <codexscript1@gmail.com>
2020-02-01 15:38:00 -0500

    stuff guys

---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    private int ScoreToLevelUp { get; set; } = 10;
+    private int CurrentScore { get; set; }
+    private int CurrentLevel { get; set; } = 1;
+    public void OnCollisionEnter(Collision other)
+    {
+        Collectable obj;
+        if ((obj = other.collider.GetComponent<Collectable>()) != null && obj.levelNeededToCollect <= CurrentLevel)
+        {
+            obj.Collect(gameObject);
+            CurrentScore += 1;
+
+            if (CurrentScore > ScoreToLevelUp)
+            {
+                CurrentLevel++;
+                CurrentScore = 0;
+            }
+        }
+    }
+}
