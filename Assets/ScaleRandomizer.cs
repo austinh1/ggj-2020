4d352d2a9a595c2191ffa258cbd8481afa259ccc
austinh1 <codexscript1@gmail.com>
2020-02-02 15:05:57 -0500

    Grass!

--- Assets/ScaleRandomizer.cs
+++ Assets/ScaleRandomizer.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ScaleRandomizer : MonoBehaviour
 {
-    
+    private GameObject Player { get; set; }
+    [SerializeField] private GameObject visual;
     void Start()
     {
+        Player = GameObject.FindWithTag("Player");
+
         var scale = Random.Range(.5f, 2f);
         
         transform.localScale = new Vector3(scale, scale, scale);
